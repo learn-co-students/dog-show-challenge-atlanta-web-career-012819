20cc258fe6e66a57d366e9b66b4f790da444551e
Megan Fago <meganlaynefago@gmail.com>
2019-03-17 21:27:35 -0400

    first attempt at the table.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
+// I made these. I noticed every lab in the past (booklist, pokemon, toytale)
+const BASE_URL = "http://localhost:3000"
+let dogTable = document.getElementById("table-body");
 
-})
+// this was here, I just added renderAllDogs
+ document.addEventListener('DOMContentLoaded', () => {
+renderAllDogs()
+ })
+
+// this is where it gets wild. The readMe wants this info in a table.
+// the array prints out to the console, but nothing changes 
+// on the page
+function renderAllDogs() {
+    fetch(`${BASE_URL}/dogs`)
+    .then(resp => resp.json())
+    .then(data => {console.log(data)
+        data.forEach(function(dog){
+            const tableRow = document.createElement('tr')
+            const tableData = document.createElement('td')
+            tableRow.setAttribute('class', 'tablerow')
+            tableData.setAttribute('class', 'tabledata')
+            tableRow.dataset.id = dog.id;
+            tableData.textContent = dog.name;
+            tableData.textContent = dog.breed;
+            tableData.textContent = dog.sex;
+            dogTable.appendChild(tableRow);
+            dogTable.appendChild(tableData);
+        })
+    }) 
+}
+
